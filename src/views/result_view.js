10e9633515877d62bac9c28fe49ec562ac926cee
hannahgreen1 <hannahgreen94@icloud.com>
2018-06-26 15:55:05 +0100

    views and modesl done

---
+++ src/views/result_view.js
@@ -0,0 +1,8 @@
+const PubSub = require("../helpers/pub_sub.js");
+
+
+const ResultInstrument = function (){
+};
+
+
+module.exports = ResultInstrument;
---
+++ src/views/select_view.js
@@ -0,0 +1,7 @@
+const PubSub = require("../helpers/pub_sub.js");
+
+const SelectInstrument = function (){
+};
+
+
+module.exports = SelectInstrument;
